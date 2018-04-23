c4bd156c615c4343f4fbacc2960d593dc04604b2
joonas <joonas.muukkonen@gmail.com>
2018-04-23 15:26:44 +0300

    Main Menu & Scene switching
    Added Main Menu and Scene swtiching functionality

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,7 +11,9 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
         if (Input.GetAxis("Horizontal") != 0)
             transform.position += new Vector3(Input.GetAxis("Horizontal"), 0);
-	}
+        if (Input.GetAxis("Vertical") != 0)
+            transform.position += new Vector3(0, Input.GetAxis("Vertical"));
+    }
     
     public void Move(Vector2 direction)
     {
