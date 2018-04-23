794ee0993c5b51118d9a89857627d5cc668bd902
joonas <joonas.muukkonen@gmail.com>
2018-04-23 15:36:42 +0300

    More Prefabs
    Added prefabs for Main Menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,8 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    // Menu functionality for starting the game.
     public void StartGame()
     {
         SceneManager.LoadScene("Main");
+    }
+
+    // Menu functionality for exiting the game.
+    public void Exit()
+    {
+        Application.Quit();
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-
-	void Start () {
+public class PlayerController : MonoBehaviour
+{
+	void Start ()
+    {
 		
 	}
 	
-	void Update () {
+	void Update ()
+    {
         if (Input.GetAxis("Horizontal") != 0)
-            transform.position += new Vector3(Input.GetAxis("Horizontal"), 0);
+            transform.position += new Vector3(Input.GetAxis("Horizontal") * 0.2f, 0);
         if (Input.GetAxis("Vertical") != 0)
-            transform.position += new Vector3(0, Input.GetAxis("Vertical"));
+            transform.position += new Vector3(0, Input.GetAxis("Vertical") * 0.2f);
     }
     
     public void Move(Vector2 direction)
