a3a0671a42fa35a980604f4d811b9a5993c348be
joonas <joonas.muukkonen@gmail.com>
2018-04-24 08:24:34 +0300

    Added UI Controls

---
+++ Assets/Editor Scripts/EditorShortcuts.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class EditorShortcuts : ScriptableObject
+{
+    // New editor shortcut for F5, binding it to the Edit/Play Menu Item.
+    [MenuItem("Edit/Run _F5")]
+    static void PlayGame()
+    {
+        // Saves the scene before executing the command.
+        if (!Application.isPlaying)
+            EditorSceneManager.SaveScene(SceneManager.GetActiveScene(), "", false);
+
+        EditorApplication.ExecuteMenuItem("Edit/Play");
+    }
+}
